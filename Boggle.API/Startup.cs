824610c4c967d379dafeccc929e0ab4bdf16e628
Goran Bunjevac <bunjevacgoran@gmail.com>
2020-12-06 12:26:02 +0100

    Add cors config

--- Boggle.API/Startup.cs
+++ Boggle.API/Startup.cs
@@ -22,6 +22,8 @@ namespace Boggle.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
+
             services.AddControllers();
 
             services.AddScoped<IScoreService, ScoreService>();
@@ -46,6 +48,8 @@ namespace Boggle.API
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder => { builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); });
 
             app.UseHttpsRedirection();
 
