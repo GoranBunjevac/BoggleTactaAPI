6e3b7ebe3091544036b42264f55c67a623728ff2
Goran Bunjevac <bunjevacgoran@gmail.com>
2020-12-06 16:27:39 +0100

    Refactor switch case

--- Boggle.API/Helpers/WordPointsCalculator.cs
+++ Boggle.API/Helpers/WordPointsCalculator.cs
@@ -23,20 +23,15 @@
                 }
                 else
                 {
-                    switch (trimmedWord.Length)
+                    return trimmedWord.Length switch
                     {
-                        case 3:
-                        case 4:
-                            return 1;
-                        case 5:
-                            return 2;
-                        case 6:
-                            return 3;
-                        case 7:
-                            return 5;
-                        default:
-                            return 0;
-                    }
+                        3 => 1,
+                        4 => 1,
+                        5 => 2,
+                        6 => 3,
+                        7 => 5,
+                        _ => 0
+                    };                 
                 }
             }
         }
